a8c611a063c0f2022923884dcd49e546f22b019c
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-05-23 13:57:01 +0100

    [Feature/Alerts: Added Health and error triggers]

---
+++ src/SubmissionCheckSplitter.Functions/V1/HealthHttpTrigger.cs
@@ -0,0 +1,17 @@
+﻿namespace SubmissionCheckSplitter.Functions.V1
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+
+    public static class HealthHttpTrigger
+    {
+        [FunctionName(nameof(HealthHttpTrigger))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "admin/health")] HttpRequest req, int? code)
+        {
+            return new OkObjectResult("Healthy");
+        }
+    }
+}
