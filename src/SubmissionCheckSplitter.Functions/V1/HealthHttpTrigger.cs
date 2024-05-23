f1e6f0cfe73e218a5b3379e06de20b2e0984a014
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-05-23 15:06:21 +0100

    [Feature/Alerts: Refactored the HealthHttpTrigger and removed the ErrorHttpTrigger]

--- src/SubmissionCheckSplitter.Functions/V1/HealthHttpTrigger.cs
+++ src/SubmissionCheckSplitter.Functions/V1/HealthHttpTrigger.cs
@@ -5,12 +5,21 @@
     using Microsoft.Azure.WebJobs;
     using Microsoft.Azure.WebJobs.Extensions.Http;
 
+    /// <summary>
+    /// Health check endpoint "api/health{code}".
+    /// "/api/health" will return 200 OK, "/api/health/500" will return 500 Internal Serer Error, etc.
+    /// </summary>
     public static class HealthHttpTrigger
     {
         [FunctionName(nameof(HealthHttpTrigger))]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "admin/health")] HttpRequest req, int? code)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "health/{code:int}")] HttpRequest req, int? code)
         {
+            if (code != null)
+            {
+                return new ObjectResult($"Specific code response for {code}") { StatusCode = code };
+            }
+
             return new OkObjectResult("Healthy");
         }
     }
