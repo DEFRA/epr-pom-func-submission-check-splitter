50fcb4ee7e0e865e1cc1aeca404ea40dfdf47357
To-Sanh Nim <to-sanh.nim@atos.net>
2025-10-23 10:15:50 +0100

    Add resilience

--- src/SubmissionCheckSplitter.Functions/Startup.cs
+++ src/SubmissionCheckSplitter.Functions/Startup.cs
@@ -2,7 +2,11 @@
 using System.Net.Http.Headers;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
 using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Retry;
+using Polly.Timeout;
 using SubmissionCheckSplitter.Application.Clients;
 using SubmissionCheckSplitter.Application.Clients.Interfaces;
 using SubmissionCheckSplitter.Application.Extensions;
@@ -33,7 +37,8 @@ public class Startup : FunctionsStartup
             var submissionApiConfig = sp.GetRequiredService<IOptions<SubmissionApiConfig>>().Value;
             c.BaseAddress = new Uri(submissionApiConfig.BaseUrl);
             c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        });
+        })
+        .AddResilienceHandler("SubmissionApiResiliencePipeline", BuildResiliencePipeline());
 
         services.AddHttpClient<IValidationDataApiClient, ValidationDataApiClient>((sp, c) =>
         {
@@ -42,6 +47,50 @@ public class Startup : FunctionsStartup
             c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             c.Timeout = TimeSpan.FromSeconds(validationDataApiConfig.Timeout);
         })
-        .AddHttpMessageHandler<ValidationDataApiAuthorisationHandler>();
+        .AddHttpMessageHandler<ValidationDataApiAuthorisationHandler>()
+        .AddResilienceHandler("ValidationDataResiliencePipeline", BuildResiliencePipeline<ValidationDataApiConfig>(o => TimeSpan.FromSeconds(o.Timeout)));
+    }
+
+    private static Action<ResiliencePipelineBuilder<HttpResponseMessage>> BuildResiliencePipeline() =>
+            builder => BuildResiliencePipeline(builder);
+
+    private static Action<ResiliencePipelineBuilder<HttpResponseMessage>, ResilienceHandlerContext> BuildResiliencePipeline<TConfig>(Func<TConfig, TimeSpan> timeoutSelector)
+        where TConfig : class =>
+        (builder, context) =>
+        {
+            var sp = context.ServiceProvider;
+            var timeout = timeoutSelector(sp.GetRequiredService<IOptions<TConfig>>()?.Value);
+            BuildResiliencePipeline(builder, timeout);
+        };
+
+    private static void BuildResiliencePipeline(ResiliencePipelineBuilder<HttpResponseMessage> builder, TimeSpan? timeout = null)
+    {
+        builder.AddRetry(new HttpRetryStrategyOptions
+        {
+            MaxRetryAttempts = 4,
+            Delay = TimeSpan.FromSeconds(2),
+            BackoffType = DelayBackoffType.Exponential,
+            ShouldHandle = (RetryPredicateArguments<HttpResponseMessage> args) =>
+            {
+                bool shouldHandle;
+                var exception = args.Outcome.Exception;
+                if (exception is TimeoutRejectedException ||
+                   (exception is OperationCanceledException && exception.Source == "System.Private.CoreLib" && exception.InnerException is TimeoutException))
+                {
+                    shouldHandle = true;
+                }
+                else
+                {
+                    shouldHandle = HttpClientResiliencePredicates.IsTransient(args.Outcome);
+                }
+
+                return new ValueTask<bool>(shouldHandle);
+            },
+        });
+
+        if (timeout is not null)
+        {
+            builder.AddTimeout(timeout.Value);
+        }
     }
 }
